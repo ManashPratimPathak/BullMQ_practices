b89cf48849d7d1e842a54ee9bc0659dd1a30065d
ManashPratimPathak <mppoff021@gmail.com>
2024-01-13 23:09:53 +0530

    create sample producer and add gitignore

--- producer.js
+++ producer.js
@@ -1,1 +1,12 @@
+const { Queue } = require("bullmq");
+const notificationQueue = new Queue("email-queue");
+async function init() {
+  const res = await notificationQueue.add("email to manash", { 
+    email: "mppoff021@gmai.com",
+    subject: "Welcome Manash",
+    body: "Hey Manash, Welcome", 
+  });
+  console.log('Job added to the queue', res.id);
+}
 
+init();
