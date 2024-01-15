dafda51965f3fa485af5e182f63573507d63da7e
ManashPratimPathak <mppoff021@gmail.com>
2024-01-15 23:44:23 +0530

    add worker code

--- worker.js
+++ worker.js
@@ -1,1 +1,11 @@
+const {Worker} = require('bullmq');
+// telling the worker that which queue are you part of!
+const sendEmail = () => new Promise((res,rej)=>setTimeout(()=> res(), 5*1000));
+const worker = new Worker("email-queue", async (job)=>{
+    console.log(`Message recived id: ${job.id}`);
+    console.log(`Processing Message`);
+    console.log(`Sending email to ${job.data.email}`);
+    await sendEmail();
+    console.log("Email Sent"); 
+}).run();
 
